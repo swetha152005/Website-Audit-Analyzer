d79f828348eb9dad83675a4210baeae8962769c6
swetha152005 <swethamarirajan@gmail.com>
2026-07-25 14:17:52 +0530

    Add footer credit link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -196,9 +196,17 @@ const analyzeWebsite = async () => {
 
 
 
-      <footer>
-        Built for Digital Heroes Training Task 🚀
-      </footer>
+    <footer>
+       <p>
+    <a 
+      href="https://digitalheroesco.com"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Built for Digital Heroes Training Task
+    </a>
+    </p>
+    </footer>
 
 
     </div>
